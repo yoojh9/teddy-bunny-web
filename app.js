6165293b5e4d99cc2d4d15838f1add9a5ababb24
Jeonghyun <yoojh9@gmail.com>
2020-02-24 21:15:27 +0900

    add ejs view engine

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
+var express = require("express");
+var httpError = require("http-errors");
+var path = require("path");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var app = express();
 
-var express = require('express')
-var httpError = require('http-errors')
-var path = require('path')
-var bodyParser = require('body-parser')
-var logger = require('morgan')
+const PORT = process.env.PORT || 3000;
 
-var app = express()
+var indexRouter = require("./routes/index");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "html");
-
+app.set("view engine", "ejs");
 
 app.use(logger("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(httpError(404));
 });
-
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  console.error(err);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(PORT, function() {
+  console.log("Example skill server listening on port " + PORT);
 });
 
 module.exports = app;
